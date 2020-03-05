0ad804036bae5d616e3f83439f63f6a9b44575b6
Oscar Alvarez <Oscar.Alvarez@teaminternational.com>
2020-03-05 11:45:26 -0500

    chrome headless

---
+++ protractor/headless.config.ts
@@ -0,0 +1,19 @@
+import { Config, browser } from 'protractor';
+import { reporter } from './helpers/reporter';
+
+export const config: Config = {
+  framework: 'jasmine',
+  SELENIUM_PROMISE_MANAGER: false,
+  specs: [ '../test/google.spec.js' ],
+  onPrepare: () => {
+    reporter();
+    browser.ignoreSynchronization = true;
+  },
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu']
+    }
+  }
+};
+
