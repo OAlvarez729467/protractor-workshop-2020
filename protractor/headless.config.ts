13e88aa79eb69926dd1b7ce5c918e893b50cf8de
Oscar Alvarez <Oscar.Alvarez@teaminternational.com>
2020-03-05 14:03:36 -0500

    se agrega el analisis de codigo

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -4,7 +4,7 @@ import { reporter } from './helpers/reporter';
 export const config: Config = {
   framework: 'jasmine',
   SELENIUM_PROMISE_MANAGER: false,
-  specs: [ '../test/google.spec.js' ],
+  specs: ['../test/google.spec.js'],
   onPrepare: () => {
     reporter();
     browser.ignoreSynchronization = true;
@@ -16,4 +16,3 @@ export const config: Config = {
     }
   }
 };
-
