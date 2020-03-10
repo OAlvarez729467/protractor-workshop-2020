2901e8effb0efdd988afcbfdc2d0603c57155c2e
Oscar Alvarez <Oscar.Alvarez@teaminternational.com>
2020-03-10 13:12:21 -0500

    configuracion de describes

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -4,31 +4,51 @@ import { MenuContentPage, ProductAddedPage, ProductListPage,
   ShippingPage, PaymentPage, SummaryPage} from '../src/page';
 
 describe('Buy a t-shirt', () => {
-  const menuContentPage: MenuContentPage = new MenuContentPage();
-  const productListPage:  ProductListPage = new ProductListPage();
-  const productAddedPage:  ProductAddedPage = new ProductAddedPage();
-  const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
-  const signInPage: SignInPage = new SignInPage();
-  const addressPage: AddressPage = new AddressPage();
-  const shippingPage: ShippingPage = new ShippingPage();
-  const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
-  const summaryPage: SummaryPage = new SummaryPage();
-  const paymentPage: PaymentPage = new PaymentPage();
 
-  it('then should be bought a t-shirt', async () => {
-    await browser.get('http://automationpractice.com/');
-    await menuContentPage.goToTShirtMenu();
-    await productListPage.addProduct();
-    await productAddedPage.checkoutProduct();
-    await orderSummaryPage.proceedCheckout();
-    await signInPage.emailFill('aperdomobo@gmail.com');
-    await signInPage.passwordFill('WorkshopProtractor');
-    await signInPage.signIn();
-    await addressPage.proceedAddress();
-    await shippingPage.agreeTerms();
-    await shippingPage.proceedShipping();
-    await bankPaymentPage.payBankOption();
-    await summaryPage.confirmOrder();
-    await paymentPage.payBankOption('Your order on My Store is complete.');
+  describe('Given a shopping page', () => {
+    beforeAll(async () => {
+      await browser.get('http://automationpractice.com/');
+    });
+
+    describe('When buy a T-Shirt', () => {
+      beforeAll(async () => {
+        const menuContentPage: MenuContentPage = new MenuContentPage();
+        const productListPage:  ProductListPage = new ProductListPage();
+        const productAddedPage:  ProductAddedPage = new ProductAddedPage();
+        const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
+        await menuContentPage.goToTShirtMenu();
+        await productListPage.addProduct();
+        await productAddedPage.checkoutProduct();
+        await orderSummaryPage.proceedCheckout();
+      });
+
+      describe('and sing-in in the page', () => {
+        beforeAll(async () => {
+          const signInPage: SignInPage = new SignInPage();
+          await signInPage.emailFill('aperdomobo@gmail.com');
+          await signInPage.passwordFill('WorkshopProtractor');
+          await signInPage.signIn();
+        });
+
+        describe('and select the default address', () => {
+          beforeAll(async () => {
+            const addressPage: AddressPage = new AddressPage();
+            const shippingPage: ShippingPage = new ShippingPage();
+            await addressPage.proceedAddress();
+            await shippingPage.agreeTerms();
+            await shippingPage.proceedShipping();
+          });
+
+          it('then should be bought a t-shirt', async () => {
+            const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
+            const summaryPage: SummaryPage = new SummaryPage();
+            const paymentPage: PaymentPage = new PaymentPage();
+            await bankPaymentPage.payBankOption();
+            await summaryPage.confirmOrder();
+            await paymentPage.payBankOption('Your order on My Store is complete.');
+          });
+        });
+      });
+    });
   });
 });
